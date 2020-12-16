765b9f0b8a93eb8b7377041a57ed67ea01c16381
yashikabihani <51638501+yashikabihani@users.noreply.github.com>
2020-12-16 19:35:45 +0530

    Fetching Photos from the url

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# PicSome
-Photo Commerce Site
+Pic Some
+
+# A little mock E-commerce Site.
+The idea is for the user to visit our site, look at the pictures available and choose some of them to be printed and shipped to them.
+
+#Images from our list can be favourite-d and can also be added to the cart!
+The great things about the project is, it will touch upon :
+* React Router
+* React Hooks
+* Custom Hooks
+* Context
+* propTypes
---
+++ src/Context.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from "react"
+
+const Context = React.createContext()
+
+function ContextProvider({children}){
+    const [allPhotos, setAllPhotos] = useState([])
+
+    const url = "https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json"
+    useEffect(() => {
+        fetch(url)
+            .then(res => res.json())
+            .then(data => setAllPhotos(data))
+    },[])
+
+    // console.log(allPhotos)
+
+    return(
+        <Context.Provider value={{allPhotos}}>
+            {children}
+        </Context.Provider>
+    )
+}
+
+export {ContextProvider, Context}
